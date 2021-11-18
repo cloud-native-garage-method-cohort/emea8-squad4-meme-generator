b9e94c48938ddb697c9870fee5fc9c095149cf7a
fred.aklamanu@gmail.com <fred.aklamanu@gmail.com>
2021-11-18 14:17:44 +0100

    Meme generator application

--- src/App.js
+++ src/App.js
@@ -1,25 +1,94 @@
-import logo from './logo.svg';
+import React from 'react';
+import ReactDOM from 'react-dom';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <div>
+          <Header />
+          <MemeGenerator />
+      </div>
+  )
 }
+
+function Header() {
+  return (
+      <header>
+          <img 
+              src="http://www.pngall.com/wp-content/uploads/2016/05/Trollface.png" 
+              alt="Problem?"
+          />
+          <p>Squad 4 Meme Generator</p>
+      </header>
+  )
+}
+
+class MemeGenerator extends React.Component {
+  constructor() {
+      super()
+      this.state = {
+          topText: "",
+          bottomText: "",
+          randomImg: "http://i.imgflip.com/1bij.jpg",
+          allMemeImgs: []
+      }
+      this.handleChange = this.handleChange.bind(this)
+      this.handleSubmit = this.handleSubmit.bind(this)
+  }
+  
+  componentDidMount() {
+      fetch("https://api.imgflip.com/get_memes")
+          .then(response => response.json())
+          .then(response => {
+              const {memes} = response.data
+              this.setState({ allMemeImgs: memes })
+          })
+  }
+  
+  handleChange(event) {
+      const {name, value} = event.target
+      this.setState({ [name]: value })
+  }
+  
+  handleSubmit(event) {
+      event.preventDefault()
+      const randNum = Math.floor(Math.random() * this.state.allMemeImgs.length)
+      const randMemeImg = this.state.allMemeImgs[randNum].url
+      this.setState({ randomImg: randMemeImg })
+  }
+  
+  render() {
+      return (
+          <div>
+              <form className="meme-form" onSubmit={this.handleSubmit}>
+                  <input 
+                      type="text"
+                      name="topText"
+                      placeholder="Top Text"
+                      value={this.state.topText}
+                      onChange={this.handleChange}
+                  /> 
+                  <input 
+                      type="text"
+                      name="bottomText"
+                      placeholder="Bottom Text"
+                      value={this.state.bottomText}
+                      onChange={this.handleChange}
+                  /> 
+              
+                  <button>Gen</button>
+              </form>
+              <div className="meme">
+                  <img src={this.state.randomImg} alt="" />
+                  <h2 className="top">{this.state.topText}</h2>
+                  <h2 className="bottom">{this.state.bottomText}</h2>
+              </div>
+          </div>
+      )
+  }
+}
+
+
+ReactDOM.render(<App />, document.getElementById("root"))
 
 export default App;
