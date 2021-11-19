434e142de18616c0cac39265ffe205ef0ada332c
Lev.Andrejev@ibm.com <Lev.Andrejev@ibm.com>
2021-11-19 13:24:16 +0100

    Add fetch to route

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./App.css";
+import { useAxios } from "use-axios-client";
 
 function App() {
   return (
@@ -37,7 +38,10 @@ class MemeGenerator extends React.Component {
   }
 
   componentDidMount() {
-    fetch("squad4-meme-generator-backend-svc/memes") //<Service-name>/memes
+    // fetch("squad4-meme-generator-backend-svc/memes") //<Service-name>/memes
+    fetch(
+      "http://squad4-meme-generator-backend-svc-squad4-meme-generator.itzroks-100000kr1k-aszhba-6ccd7f378ae819553d37d5f2ee142bd6-0000.eu-gb.containers.appdomain.cloud/memes"
+    ) //<Route-name>/memes
       .then((response) => response.json())
       .then((response) => {
         const { memes } = response.data;
