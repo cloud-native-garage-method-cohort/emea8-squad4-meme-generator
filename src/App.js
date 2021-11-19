2c2e0b976d12fd5ef56b7aa7efcbcfaa5d17beda
Lev.Andrejev@ibm.com <Lev.Andrejev@ibm.com>
2021-11-19 12:22:09 +0100

    Use BE url

--- src/App.js
+++ src/App.js
@@ -1,94 +1,93 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './App.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./App.css";
 
 function App() {
   return (
-      <div>
-          <Header />
-          <MemeGenerator />
-      </div>
-  )
+    <div>
+      <Header />
+      <MemeGenerator />
+    </div>
+  );
 }
 
 function Header() {
   return (
-      <header>
-          <img 
-              src="http://www.pngall.com/wp-content/uploads/2016/05/Trollface.png" 
-              alt="Problem?"
-          />
-          <p>Squad 4 Meme Generator</p>
-      </header>
-  )
+    <header>
+      <img
+        src="http://www.pngall.com/wp-content/uploads/2016/05/Trollface.png"
+        alt="Problem?"
+      />
+      <p>Squad 4 Meme Generator</p>
+    </header>
+  );
 }
 
 class MemeGenerator extends React.Component {
   constructor() {
-      super()
-      this.state = {
-          topText: "",
-          bottomText: "",
-          randomImg: "http://i.imgflip.com/1bij.jpg",
-          allMemeImgs: []
-      }
-      this.handleChange = this.handleChange.bind(this)
-      this.handleSubmit = this.handleSubmit.bind(this)
+    super();
+    this.state = {
+      topText: "",
+      bottomText: "",
+      randomImg: "http://i.imgflip.com/1bij.jpg",
+      allMemeImgs: [],
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
-  
+
   componentDidMount() {
-      fetch("https://api.imgflip.com/get_memes")
-          .then(response => response.json())
-          .then(response => {
-              const {memes} = response.data
-              this.setState({ allMemeImgs: memes })
-          })
+    fetch("squad4-meme-generator-backend-svc/memes") //<Service-name>/memes
+      .then((response) => response.json())
+      .then((response) => {
+        const { memes } = response.data;
+        this.setState({ allMemeImgs: memes });
+      });
   }
-  
+
   handleChange(event) {
-      const {name, value} = event.target
-      this.setState({ [name]: value })
+    const { name, value } = event.target;
+    this.setState({ [name]: value });
   }
-  
+
   handleSubmit(event) {
-      event.preventDefault()
-      const randNum = Math.floor(Math.random() * this.state.allMemeImgs.length)
-      const randMemeImg = this.state.allMemeImgs[randNum].url
-      this.setState({ randomImg: randMemeImg })
+    event.preventDefault();
+    const randNum = Math.floor(Math.random() * this.state.allMemeImgs.length);
+    const randMemeImg = this.state.allMemeImgs[randNum].url;
+    this.setState({ randomImg: randMemeImg });
   }
-  
+
   render() {
-      return (
-          <div>
-              <form className="meme-form" onSubmit={this.handleSubmit}>
-                  <input 
-                      type="text"
-                      name="topText"
-                      placeholder="Top Text"
-                      value={this.state.topText}
-                      onChange={this.handleChange}
-                  /> 
-                  <input 
-                      type="text"
-                      name="bottomText"
-                      placeholder="Bottom Text"
-                      value={this.state.bottomText}
-                      onChange={this.handleChange}
-                  /> 
-              
-                  <button>Gen</button>
-              </form>
-              <div className="meme">
-                  <img src={this.state.randomImg} alt="" />
-                  <h2 className="top">{this.state.topText}</h2>
-                  <h2 className="bottom">{this.state.bottomText}</h2>
-              </div>
-          </div>
-      )
+    return (
+      <div>
+        <form className="meme-form" onSubmit={this.handleSubmit}>
+          <input
+            type="text"
+            name="topText"
+            placeholder="Top Text"
+            value={this.state.topText}
+            onChange={this.handleChange}
+          />
+          <input
+            type="text"
+            name="bottomText"
+            placeholder="Bottom Text"
+            value={this.state.bottomText}
+            onChange={this.handleChange}
+          />
+
+          <button>Gen</button>
+        </form>
+        <div className="meme">
+          <img src={this.state.randomImg} alt="" />
+          <h2 className="top">{this.state.topText}</h2>
+          <h2 className="bottom">{this.state.bottomText}</h2>
+        </div>
+      </div>
+    );
   }
 }
 
-
-ReactDOM.render(<App />, document.getElementById("root"))
+ReactDOM.render(<App />, document.getElementById("root"));
 
 export default App;
