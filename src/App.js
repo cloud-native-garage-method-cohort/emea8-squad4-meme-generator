f7923955dd40d903aeae183a25484d4f18d8e345
Lev.Andrejev@ibm.com <Lev.Andrejev@ibm.com>
2021-11-19 14:34:51 +0100

    revert api

--- src/App.js
+++ src/App.js
@@ -37,9 +37,8 @@ class MemeGenerator extends React.Component {
   }
 
   componentDidMount() {
-    fetch(
-      "https://squad4-meme-generator-backend-svc-squad4-meme-generator.itzroks-100000kr1k-aszhba-6ccd7f378ae819553d37d5f2ee142bd6-0000.eu-gb.containers.appdomain.cloud/memes"
-    )
+    // fetch("http://squad4-meme-generator-backend-svc-squad4-meme-generator.itzroks-100000kr1k-aszhba-6ccd7f378ae819553d37d5f2ee142bd6-0000.eu-gb.containers.appdomain.cloud/memes")
+    fetch("https://api.imgflip.com/get_memes")
       .then((response) => response.json())
       .then((response) => {
         const { memes } = response.data;
